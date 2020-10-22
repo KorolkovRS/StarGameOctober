dd27fa7d6f60f804f686462613ed4456aed5dc6e
Alexey Kutepov <alkutepov@gmail.com>
2020-10-22 19:50:23 +0300

    Урок 4 часть 1

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,22 +1,36 @@
 package ru.geekbrains.screen;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 256;
+
+    private TextureAtlas atlas;
     private Texture bg;
+
     private Background background;
+    private Star[] stars;
+
 
     @Override
     public void show() {
         super.show();
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
         bg = new Texture("textures/bg.png");
+
         background = new Background(bg);
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < STAR_COUNT; i++) {
+            stars[i] = new Star(atlas);
+        }
     }
 
     @Override
@@ -29,11 +43,15 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
@@ -43,11 +61,17 @@ public class MenuScreen extends BaseScreen {
     }
 
     private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
         batch.end();
     }
 }
