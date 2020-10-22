3ad1b5e21eca86a86a2878297a60ce1dc8d9179c
Alexey Kutepov <alkutepov@gmail.com>
2020-10-22 19:17:55 +0300

    Урок 4 ДЗ

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,46 +1,63 @@
 package ru.geekbrains.screen;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
+    private Texture img;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
-        background = new Background(new TextureRegion(bg));
+        background = new Background(bg);
+        img = new Texture("badlogic.jpg");
+        logo = new Logo(img);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
-        background.draw(batch);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        img.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return false;
+    }
+
+    private void update(float delta) {
+        logo.update(delta);
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        logo.draw(batch);
+        batch.end();
     }
 }
