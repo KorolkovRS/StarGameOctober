bac1c5a2b8e4157e021f949072c6269679da6f66
Alexey Kutepov <alkutepov@gmail.com>
2020-10-22 19:24:18 +0300

    Урок 4 удалил лишнее + утилиты

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -6,22 +6,17 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
-import ru.geekbrains.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
-    private Texture img;
-    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
-        img = new Texture("badlogic.jpg");
-        logo = new Logo(img);
     }
 
     @Override
@@ -34,30 +29,25 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
-        logo.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         bg.dispose();
-        img.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        logo.touchDown(touch, pointer, button);
         return false;
     }
 
     private void update(float delta) {
-        logo.update(delta);
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
-        logo.draw(batch);
         batch.end();
     }
 }
